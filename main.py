38874898060470d8c27a20ba8f31d4a0d2648f8c
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-11-03 10:20:44 -0800

    descriptions are mostly fixed

--- main.py
+++ main.py
@@ -2,24 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 def getForumHome():
-    request = requests.get("https://scratch.mit.edu/discuss")
+    request = requests.get("https://scratch.mit.edu/discuss", verify=False) #because of the stupid BYOD
     req_text = request.text
-    soup = BeautifulSoup(req_text)
+    soup = BeautifulSoup(req_text, features="html.parser")
     #print(soup)
     categories = []
     # Let's get the forums first.
     idx1 = soup.select_one("#idx1") #get the container with the categories
     for category in idx1.select(".box"):
-        category_name = category.select_one(".box-head h4").text
+        #forum_tr.select_one("tr .tcl .intd .tclcon h3").text.split("\n")[0]
+        category_name = category.select_one(".box-head h4").text.split("\nToggle shoutbox\n                ")[1].split("\n")[0]
         category_dict = {
             "category_name": category_name,
             "forums": []
         }
         forum_tbody = category.select(".box-content table tbody")
         for forum_tr in forum_tbody:
-            forum_name = forum_tr.select_one("tr .tcl .intd .tclcon h3")
+            forum_name = forum_tr.select_one("tr .tcl .intd .tclcon h3").text.split("\n")[0]
             forum_id = forum_tr.select_one("tr .tcl .intd .tclcon h3 a").get("href").split("/discuss/")[1].split("/")[0]
-            forum_desc = forum_tr.select_one("tr .tcl .intd .tclcon").text
+            forum_desc_tag = forum_tr.select_one("tr .tcl .intd .tclcon")
+            print("oegoirg")
+            print(forum_desc_tag)
+            try:
+                forum_desc = forum_desc_tag.contents[-1].text.split("\n                ")[1]
+            except IndexError:
+                forum_desc = ""
+            print(forum_desc)
             topic_count = forum_tr.select_one("tr .tc2").text
             post_count = forum_tr.select_one("tr .tc3").text
             forum_dict = {
@@ -34,10 +42,3 @@ def getForumHome():
     print(categories)
 
 
-
-
-
-print("Scratch Forum CLI-ent")
-print("for the Midnight challenge")
-
-getForumHome()
