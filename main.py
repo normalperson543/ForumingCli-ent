49644766446d1250d3f8fbac9651635af9d4848e
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-11-05 01:33:29 -0800

    more preprocessing + better input system

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from termcolor import colored, cprint
+import os
 
 def get_forum_home():
     print(colored("Get forum homepage", "grey"))
@@ -130,17 +131,23 @@ def print_topic(topic):
         text = text.replace('<div style="text-align:center;">', "") #unsupported
         text = text.replace('<span class="bb-big">', "")  # unsupported
         text = text.replace('<span class="bb-small">', "")  # unsupported
+        text = text.replace('<p class="bb-quote-author">', "")  # unsupported
         text = text.replace('<ul>', "")  # unsupported
         text = text.replace('</ul>', "")  # unsupported
+        text = text.replace('<img src="', "")  # just give the link
+        text = text.replace('"/>', "")  # just give the link
         text = text.replace('<blockquote>', "")  # unsupported (for now)
         text = text.replace('</blockquote>', "\n")
         text = text.replace('<li>', "* ")  # bulleted stuff will be replaced with a dot
         text = text.replace('</li>', "")
         text = text.replace('<span class="bb-bold">', "\033[1m")
-        text = text.replace('<p class="bb-quote-author">', "\033[1m") #the quote author will be bolded, just like in web
-        text = text.replace('<span class="bb-italic">', "\x1B[3m")  # unsupported
+        text = text.replace('<span class="bb-underline">', "\033[4m")
+        text = text.replace('<span class="bb-italic">', "\x1B[3m")
+        text = text.replace('<em>', "\x1B[3m")  # unsupported
         text = text.replace('</span>', "\033[0m\x1B[0m")
+        text = text.replace('</em>', "\x1B[0m")
         text = text.replace('</p>', "")
+        text = text.replace('<span>', "")
         text = text.replace('<a href="', " ") #remove link tag (but leave a space)
         text = text.replace('">', "") #mmm love jank
         text = text.replace('</a>', "")
@@ -148,26 +155,77 @@ def print_topic(topic):
         print(text)
         print("\n")
 
+def accept_user_input():
+    answer = input("? ")
+    command_split = answer.split(" ")
+    global current_page
+    global previous_forum
+    if command_split[0] == "of":
+        try:
+            forum_id = int(command_split[1])
+            forum = get_forum(forum_id)
+            os.system("clear")
+            current_page = "f"
+            previous_forum = forum_id
+            print_forum(forum)
+            return
+        except ValueError:
+            print("Whoops, that's not a valid forum ID.")
+            return
+    if command_split[0] == "ot":
+        try:
+            topic_id = int(command_split[1])
+            topic = get_topic(topic_id)
+            os.system("clear")
+            current_page = "t"
+            print_topic(topic)
+            return
+        except ValueError:
+            print("Whoops, that's not a valid forum ID.")
+            return
+    if command_split[0] == "h":
+        categories = get_forum_home()
+        current_page = "h"
+        os.system("clear")
+        print_forum_info(categories)
+        return
+    if command_split[0] == "b":
+        if current_page == "t":
+            forum = get_forum(previous_forum)
+            os.system("clear")
+            current_page = "f"
+            print_forum(forum)
+            return
+        if current_page == "f":
+            categories = get_forum_home()
+            current_page = "h"
+            os.system("clear")
+            print_forum_info(categories)
+            return
+    try:
+        id = int(command_split[0])
+        if current_page == "h":
+            forum = get_forum(id)
+            os.system("clear")
+            current_page = "f"
+            previous_forum = id
+            print_forum(forum)
+            return
+        if current_page == "f":
+            topic = get_topic(id)
+            os.system("clear")
+            current_page = "t"
+            print_topic(topic)
+            return
+    except ValueError:
+        pass
+
+
 categories = get_forum_home()
+os.system("clear")
+current_page = "h"
+previous_forum = 0
 print_forum_info(categories)
 while True:
-    forum_id = input(f"Type a forum ID: ")
-    try:
-        forum_id = int(forum_id)
-        break
-    except ValueError:
-        print("Whoops, that's not a valid forum ID.")
-
-forum = get_forum(forum_id)
-print_forum(forum)
-while True:
-    topic_id = input(f"Type a topic ID: ")
-    try:
-        topic_id = int(topic_id)
-        break
-    except ValueError:
-        print("Whoops, that's not a valid topic ID.")
-
-topic = get_topic(topic_id)
-print_topic(topic)
+    accept_user_input()
 
